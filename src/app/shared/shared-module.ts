1ceb14a2dad5039886aab162df0d560c06aadf37
KimberlyFigueroa21 <kimfigueroa210004@gmail.com>
2026-03-25 10:33:04 -0500

    feat: refactor notificaciones module and add new components for shared notifications

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotificacionBadge } from './shared/notificaciones/notificacion-badge/notificacion-badge';
+import { NotificacionConfig } from './shared/notificaciones/notificacion-config/notificacion-config';
+import { NotificacionForm } from './shared/notificaciones/notificacion-form/notificacion-form';
+import { NotificacionesList } from './shared/notificaciones/notificaciones-list/notificaciones-list';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NotificacionBadge,
+    NotificacionConfig,
+    NotificacionForm,
+    NotificacionesList,
+  ],
+  exports: [
+    NotificacionBadge,
+    NotificacionConfig,
+    NotificacionForm,
+    NotificacionesList,
+  ],
 })
 export class SharedModule {}
